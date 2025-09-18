6b1da9466de38dd4a36c8c52a7cb4c9b18cdf06d
Eamon Cornilliac <eamon.cornilliac@my.uwi.edu>
2025-09-18 12:23:25 -0400

    updated User class and init methods

--- App/models/user.py
+++ App/models/user.py
@@ -5,9 +5,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
+    first_name = db.Column(db.String(32), nullable=False)
+    last_name = db.Column(db.String(32), nullable=False)
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, first_name, last_name):
         self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
         self.set_password(password)
 
     def get_json(self):
@@ -15,6 +19,9 @@ class User(db.Model):
             'id': self.id,
             'username': self.username
         }
+    
+    def __repr__(self):
+        return f'<User {self.username} - {self.first_name} - {self.last_name}>'
 
     def set_password(self, password):
         """Create hashed password."""
--- wsgi.py
+++ wsgi.py
@@ -32,8 +32,10 @@ user_cli = AppGroup('user', help='User object commands')
 @user_cli.command("create", help="Creates a user")
 @click.argument("username", default="rob")
 @click.argument("password", default="robpass")
-def create_user_command(username, password):
-    create_user(username, password)
+@click.argument("first_name")
+@click.argument("last_name")
+def create_user_command(username, password, first_name, last_name):
+    create_user(username, password, first_name, last_name)
     print(f'{username} created!')
 
 # this command will be : flask user create bob bobpass
